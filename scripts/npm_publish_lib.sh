173b42d994ee3aeda2132f361862688e31d54817
Kunal Mohta <kunalmohta1818@gmail.com>
2023-09-10 15:46:35 +0530

    fix pacakge.json, update npm publish script

--- scripts/npm_publish_lib.sh
+++ scripts/npm_publish_lib.sh
@@ -3,6 +3,11 @@
 PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
 
 cd ${PROJECT_ROOT}/packages/duckdb-wasm
+
+# Changing package name before publishing
+# This is done here, and not directly in package.json, because a lot of other scripts depend on the package name being @duckdb/duckdb-wasm
+python3 -c "import os; import json; p = json.load(open('package.json')); p['name'] = '@kunal-mohta/duckdb-wasm'; p['repository']['url'] = 'https://github.com/kunal-mohta/duckdb-wasm.git' json.dump(p, open('package.json', 'w'), indent=2, ensure_ascii=False);"
+
 mkdir -p ./dist/img
 cp ${PROJECT_ROOT}/misc/duckdb.svg ./dist/img/duckdb.svg
 cp ${PROJECT_ROOT}/misc/duckdb_wasm.svg ./dist/img/duckdb_wasm.svg
