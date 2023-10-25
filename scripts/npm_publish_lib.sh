367124035ec8a21aff8ec3c69d7c6d518ad580b8
Kunal Mohta <kunalmohta1818@gmail.com>
2023-10-25 20:32:37 +0530

    fix package version

--- scripts/npm_publish_lib.sh
+++ scripts/npm_publish_lib.sh
@@ -6,7 +6,7 @@ cd ${PROJECT_ROOT}/packages/duckdb-wasm
 
 # Changing package name before publishing
 # This is done here, and not directly in package.json, because a lot of other scripts depend on the package name being @duckdb/duckdb-wasm
-python3 -c "import os; import json; p = json.load(open('package.json')); p['name'] = '@kunal-mohta/duckdb-wasm'; p['repository']['url'] = 'https://github.com/kunal-mohta/duckdb-wasm.git'; p['version'] = '1.11.8'; json.dump(p, open('package.json', 'w'), indent=2, ensure_ascii=False);"
+python3 -c "import os; import json; p = json.load(open('package.json')); p['name'] = '@kunal-mohta/duckdb-wasm'; p['repository']['url'] = 'https://github.com/kunal-mohta/duckdb-wasm.git'; p['version'] = '1.11.9'; json.dump(p, open('package.json', 'w'), indent=2, ensure_ascii=False);"
 
 mkdir -p ./dist/img
 cp ${PROJECT_ROOT}/misc/duckdb.svg ./dist/img/duckdb.svg
